8c34418c7a394be2b25d07f9d15565eb3480e093
Ben Welman <bwelman@protonmail.com>
2023-06-09 12:41:56 +0200

    Change info help. Change name data files.

--- R/generate-zip.R
+++ R/generate-zip.R
@@ -1,4 +1,4 @@
-system("7z u -tzip Practicefiles-LearnExcel.zip -w data/.")
+system("7z u -tzip files-learnexcel.zip -w data/.")
 
 # Het zipbestand wordt alleen bijgewerkt bij
 # - wijziging bestaand bestand
